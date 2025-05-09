b6498cf72da81157e2da17ffb43294a96e3829ba
Nicholas Schell <nicholas.schell@dolby.com>
2025-05-08 19:50:39 -0700

    Add droplet destroy methods (#7)
    * Update dependencies and fix compile errors
    
    * Code enhancements based on code review.
    Minor fixes.
    
    * Tags.Create seems to require no charset, else returns a 405
    guessing other calls may have the same issue
    
    * forgot to configure with Newtonsoft serializer
    
    * up to rc2
    
    * capture data from request when throwing Exception
    
    * up to rc3
    
    * add calls for 4 destroy droplet endpoints:
    get droplet resources for usage with destroy
    destroy droplet with resources
    status destroy droplet
    retry destroy droplet
    
    ---------
    
    Co-authored-by: Ahmad Afzal Zadeh <ahmad.afzalzadeh@dolby.com>

---
+++ DigitalOcean.API/Models/Requests/DestroyResources.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace DigitalOcean.API.Models.Requests;
+
+public class DestroyResources {
+
+    // DEPRECATED
+    //[JsonProperty("floating_ips")]
+    //public List<string> FloatingIps { get; set; }
+
+    /// <summary>
+    /// An array of unique identifiers for the reserved IPs to be scheduled for deletion.
+    /// </summary>
+    [JsonProperty("reserved_ips")]
+    public List<string> ReservedIps { get; set; }
+
+    /// <summary>
+    /// An array of unique identifiers for the snapshots to be scheduled for deletion.
+    /// </summary>
+    [JsonProperty("snapshots")]
+    public List<string> Snapshots { get; set; }
+
+    /// <summary>
+    /// An array of unique identifiers for the volumes to be scheduled for deletion.
+    /// </summary>
+    [JsonProperty("volumes")]
+    public List<string> Volumes { get; set; }
+
+    /// <summary>
+    /// An array of unique identifiers for the volume snapshots to be scheduled for deletion.
+    /// </summary>
+    [JsonProperty("volume_snapshots")]
+    public List<string> VolumeSnapshots { get; set; }
+}
---
+++ DigitalOcean.API/Models/Responses/DestroyStatus.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace DigitalOcean.API.Models.Responses;
+
+public class DestroyStatus {
+
+    /// <summary>
+    /// An object containing information about a resource scheduled for deletion.
+    /// </summary>
+    [JsonProperty("droplet")]
+    public DestroyItemStatus Droplet { get; set; }
+
+    /// <summary>
+    /// An object containing additional information about resource related to a Droplet requested to be destroyed.
+    /// </summary>
+    [JsonProperty("resources")]
+    public DestroyStatusResources Resources { get; set; }
+
+    /// <summary>
+    /// A time value given in ISO8601 combined date and time format indicating when the requested action was completed.
+    /// </summary>
+    [JsonProperty("completed_at")]
+    public string CompletedAt { get; set; }
+
+    /// <summary>
+    /// A count of the associated resources that failed to be destroyed, if any.
+    /// </summary>
+    [JsonProperty("failures")]
+    public int Failures { get; set; }
+}
+
+public class DestroyItemStatus {
+
+    /// <summary>
+    /// The unique identifier for the resource scheduled for deletion.
+    /// </summary>
+    [JsonProperty("id")]
+    public string Id { get; set; }
+
+    /// <summary>
+    /// The name of the resource scheduled for deletion.
+    /// </summary>
+    [JsonProperty("name")]
+    public string Name { get; set; }
+
+    /// <summary>
+    /// A time value given in ISO8601 combined date and time format indicating when the resource was destroyed if the request was successful.
+    /// </summary>
+    [JsonProperty("destroyed_at")]
+    public string DestroyedAt { get; set; }
+
+    /// <summary>
+    /// A string indicating that the resource was not successfully destroyed and providing additional information.
+    /// </summary>
+    [JsonProperty("error_message")]
+    public string ErrorMessage { get; set; }
+}
+
+public class DestroyStatusResources {
+
+    [JsonProperty("floating_ips")]
+    public List<DestroyItemStatus> FloatingIps { get; set; }
+
+    [JsonProperty("reserved_ips")]
+    public List<DestroyItemStatus> ReservedIps { get; set; }
+
+    [JsonProperty("snapshots")]
+    public List<DestroyItemStatus> Snapshots { get; set; }
+
+    [JsonProperty("volumes")]
+    public List<DestroyItemStatus> Volumes { get; set; }
+
+    [JsonProperty("volume_snapshots")]
+    public List<DestroyItemStatus> VolumeSnapshots { get; set; }
+}
