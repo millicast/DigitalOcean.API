ffb70762203b131ed18a9c597322c47c1ac68a62
Nicholas Schell <nicholas.schell@dolby.com>
2026-02-05 02:18:38 -0800

    Fix deploy-nupkg.sh (#14)
    * switch to more compatible /usr/bin/env shebang
    
    * actually no dependency on .net8 sdk (just better to use)
    
    * specify --no-restore
    also split long lines
    
    * use /nocache parameter in gitversion

--- deploy-nupkg.sh
+++ deploy-nupkg.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -o nounset -o errexit -o pipefail
 shopt -s inherit_errexit
 
 ### REQUIREMENTS:
-### dotnet-sdk-8.0
+### dotnet-sdk-6.0 or greater
 ### jq
 
 ### SECRETS:
@@ -13,7 +13,8 @@ shopt -s inherit_errexit
 
 function main() {
   dotnet tool restore
-  versionJson="$(dotnet dotnet-gitversion /config .gitversion.yml)"
+
+  versionJson="$(dotnet dotnet-gitversion /nocache /config .gitversion.yml)"
   semVer="$(jq -r '.SemVer' <<< "${versionJson}")"
   assemblySemVer="$(jq -r '.AssemblySemVer' <<< "${versionJson}")"
   informationalVersion="$(jq -r '.InformationalVersion' <<< "${versionJson}")"
@@ -21,13 +22,19 @@ function main() {
   dotnet restore
 
   dotnet clean -c Release
-  dotnet build -c Release -p:Version="${assemblySemVer}" -p:InformationalVersion="${informationalVersion}" -p:IncludeSourceRevisionInInformationalVersion=false
 
-  dotnet test -c Release --no-build
+  dotnet build -c Release --no-restore \
+    -p:Version="${assemblySemVer}" \
+    -p:InformationalVersion="${informationalVersion}" \
+    -p:IncludeSourceRevisionInInformationalVersion=false
+
+  dotnet test -c Release --no-restore --no-build
 
   mkdir -p dist/
   rm -f dist/*
-  dotnet pack -c Release -p:PackageVersion="${semVer}" -o dist/ --no-build
+  dotnet pack -c Release --no-restore --no-build \
+    -o dist/ \
+    -p:PackageVersion="${semVer}"
 
   for nupkg in dist/*.nupkg; do
     dotnet nuget push "${nupkg}" --source "${NUPKG_PUBLISH_URL}" --api-key "${NUPKG_PUBLISH_APIKEY}"
